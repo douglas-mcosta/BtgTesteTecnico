806ca2b249acd6ce29a99cfd0925f31551955589
Douglas de Medeiros Costa <douglas.costa@confitec.com.br>
2024-10-31 21:19:02 -0300

    - Ajuste para deploy no azure

---
+++ src/web/btg-app/src/environments/environment.azure.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  IdentityUrl: "http://ecommerce-azure.brazilsouth.cloudapp.azure.com:5000/api/identidade/",
+  EcommerceUrl: "http://ecommerce-azure.brazilsouth.cloudapp.azure.com:5001/api/"
+};
